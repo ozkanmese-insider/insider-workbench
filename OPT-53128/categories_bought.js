b870fb517cabd8e3a07015b2583ad2f2c630711d
ozkanmese-insider <77895739+ozkanmese-insider@users.noreply.github.com>
2021-02-25 16:59:14 +0300

    Update categories_bought.js

--- OPT-53128/categories_bought.js
+++ OPT-53128/categories_bought.js
@@ -3,12 +3,13 @@
     var productInfosStorage = 'ins-product-infos-53128';
     var paidProductsCategories = 'ins-paid-products-category-53128';
     var categoryBought = JSON.parse(Insider.storage.localStorage.get(paidProductsCategories)) || [];
- 
+
     self.init = function () {
-        setTimeout(function () {self.checkConditions();}, 2000);
-        
+        setTimeout(function () {
+            self.checkConditions();
+        }, 2000);
     };
- 
+
     self.checkConditions = function () {
         if ((Insider.systemRules.call('isOnCartPage') || Insider.fns.hasParameter('/checkouts/'))) {
             var productUrls = Insider.storageAccessor.paidProducts().map(function (product) {
@@ -21,60 +22,58 @@
                     data: '',
                     success: function (response) {
                         var element = document.createElement('div');
-   
+
                         element.innerHTML = response.response;
-   
                         var productInfos = JSON.parse(Insider.storage.localStorage.get(productInfosStorage)) || [];
                         var categoryText = Insider.dom('.pro-meta--col.category .detail', element).text().trim();
                         var productIDText = Insider.dom('span#pro_sku', element).text().trim();
-   
                         var productDatas = {
                             cat: categoryText,
                             id: productIDText
                         };
-   
+
                         productInfos.push(productDatas);
-   
+
                         Insider.storage.set({
                             name: productInfosStorage,
                             value: JSON.stringify(productInfos)
                         });
-   
+
                         JSON.parse(Insider.storage.localStorage.get(productInfosStorage));
                     }
                 });
             });
         }
- 
+
         if (Insider.systemRules.call('isOnAfterPaymentPage')) {
             var productDatas = JSON.parse(Insider.storage.localStorage.get(productInfosStorage));
             var paidProducts = Insider.systemRules.call('getPaidProducts');
             var index;
- 
+
             for (var i =0; i < productDatas.length; i++) {
                 for (var z = 0; z < paidProducts.length; z++) {
                     if (productDatas[i].id.indexOf(paidProducts[z].id)) {
                         index = categoryBought.indexOf(productDatas[i].cat);
- 
+
                         if (index > -1) {
                             categoryBought.splice(index, 1);
                         }
- 
-                        categoryBought.push(productDatas[i].cat);                 
+
+                        categoryBought.push(productDatas[i].cat);
                     }
                 }
             }
- 
+
             Insider.storage.set({
                 name: paidProductsCategories,
                 value: JSON.stringify(categoryBought),
                 expires: Insider.dateHelper.addDay(90)
             });
-  
+
             JSON.parse(Insider.storage.localStorage.get(paidProductsCategories));
         }
     };
- 
+    
     self.init();
 })({});
 /* OPT-53128 END*/
