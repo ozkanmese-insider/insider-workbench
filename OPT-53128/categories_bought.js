26a676da0f404df717ae3bedb228dcdc8075b9e8
ozkanmese-insider <77895739+ozkanmese-insider@users.noreply.github.com>
2021-02-28 03:57:36 +0300

    update

--- OPT-53128/categories_bought.js
+++ OPT-53128/categories_bought.js
@@ -1,82 +1,106 @@
 /* OPT-53128 START*/
 (function (self) {
-    var productInfosStorage = 'ins-product-infos-53128';
-    var paidProductsCategories = 'ins-paid-products-category-53128';
-    var categoryBought = JSON.parse(Insider.storage.localStorage.get(paidProductsCategories)) || [];
+    var storageName = 'ins-purchased-control-opt53128';
+    var storageData = Insider.storage.get(storageName) || {
+        cartListOfCategory: {},
+        purchasedCategoryOfProduct: {},
+        currentCartListProductId: []
+    };
 
     self.init = function () {
-        setTimeout(function () {
-            self.checkConditions();
-        }, 2000);
+        self.setEvent();
+        self.checkCurrentState();
+        self.checkAfterPaymentCategory();
+        self.setStorage();
     };
-
-    self.checkConditions = function () {
-        if ((Insider.systemRules.call('isOnCartPage') || Insider.fns.hasParameter('/checkouts/'))) {
-            var productUrls = Insider.storageAccessor.paidProducts().map(function (product) {
-                return product.url;
+    self.setEvent = function () {
+        Insider.eventManager.off('cart:amount:update.opt53128')
+            .on('cart:amount:update.opt53128', function () {
+                setTimeout(function () {
+                    self.checkCurrentState();
+                }, 500);
             });
+    };
+    self.checkCurrentState = function () {
+        var differenceProduct = self.getDifferenceProductWithCartPage();
 
-            productUrls.forEach(function (url) {
-                Insider.request.get({
-                    url: url,
-                    data: '',
-                    success: function (response) {
-                        var element = document.createElement('div');
+        differenceProduct.forEach(function (product) {
+            var foundCategory = self.getCategoryOfProductFromStorage(product.id);
 
-                        element.innerHTML = response.response;
-                        var productInfos = JSON.parse(Insider.storage.localStorage.get(productInfosStorage)) || [];
-                        var categoryText = Insider.dom('.pro-meta--col.category .detail', element).text().trim();
-                        var productIDText = Insider.dom('span#pro_sku', element).text().trim();
-                        var productDatas = {
-                            cat: categoryText,
-                            id: productIDText
-                        };
-
-                        productInfos.push(productDatas);
-
-                        Insider.storage.set({
-                            name: productInfosStorage,
-                            value: JSON.stringify(productInfos)
-                        });
-
-                        JSON.parse(Insider.storage.localStorage.get(productInfosStorage));
-                    }
-                });
-            });
-        }
-
-        if (Insider.systemRules.call('isOnAfterPaymentPage')) {
-            var productDatas = JSON.parse(Insider.storage.localStorage.get(productInfosStorage));
-            var paidProducts = Insider.systemRules.call('getPaidProducts');
-            var index;
-
-            for (var i =0; i < productDatas.length; i++) {
-                for (var z = 0; z < paidProducts.length; z++) {
-                    if (productDatas[i].id.indexOf(paidProducts[z].id)) {
-                        index = categoryBought.indexOf(productDatas[i].cat);
-
-                        if (index > -1) {
-                            categoryBought.splice(index, 1);
-                        }
-
-                        categoryBought.push(productDatas[i].cat);
-                    }
-                }
+            if (foundCategory === '') {
+                self.sendRequest(product.url);
+            } else {
+                storageData.currentCartListProductId.push(product.id);
             }
+        });
+    };
+    self.getDifferenceProductWithCartPage = function () {
+        var cartList = Insider.storageAccessor.cartProductList().productList || [];
 
-            Insider.storage.set({
-                name: paidProductsCategories,
-                value: JSON.stringify(categoryBought),
-                expires: Insider.dateHelper.addDay(90)
+        return cartList.filter(function (product) {
+            return !storageData.currentCartListProductId.includes(product.id);
+        });
+    };
+    self.getCategoryOfProductFromStorage = function (productId) {
+        var category = '';
+
+        Object.keys(storageData.cartListOfCategory).some(function (key) {
+            if (storageData.cartListOfCategory[key].indexOf(productId) > -1) {
+                category = key;
+
+                return false;
+            }
+        });
+
+        return category;
+    };
+    self.sendRequest = function (url) {
+        Insider.request.get({
+            url: url,
+            data: '',
+            success: function (response) {
+                var element = document.createElement('div');
+
+                element.innerHTML = response.response;
+                self.storeProductInformation(element);
+            }
+        });
+    };
+    self.storeProductInformation = function (element) {
+        storageData = Insider.storage.get(storageName);
+        var category = Insider.dom('.pro-meta--col.category .detail', element).text().trim();
+        var productId = Insider.dom('span#pro_sku', element).text().trim();
+
+        typeof storageData.cartListOfCategory[category] === 'undefined' ?
+            storageData.cartListOfCategory[category] = [productId] :
+            storageData.cartListOfCategory[category].push(productId);
+        storageData.currentCartListProductId.push(productId);
+        self.setStorage();
+    };
+    self.checkAfterPaymentCategory = function () {
+        if (Insider.systemRules.call('isOnAfterPaymentPage')) {
+            var paidProducts = Insider.storageAccessor.paidProducts();
+
+            paidProducts.forEach(function (product) {
+                var category = self.getCategoryOfProductFromStorage(product.id);
+
+                typeof storageData.purchasedCategoryOfProduct[category] === 'undefined' ?
+                    storageData.purchasedCategoryOfProduct[category] = [product.id] :
+                    storageData.purchasedCategoryOfProduct[category].push(product.id);
             });
-
-            JSON.parse(Insider.storage.localStorage.get(paidProductsCategories));
         }
     };
-    
+    self.setStorage = function () {
+        Insider.storage.set({
+            name: storageName,
+            value: storageData,
+            expires: Insider.dateHelper.addDay(90)
+        });
+    };
     self.init();
 })({});
 /* OPT-53128 END*/
 
-// last_3_months_order_categories
-Insider.storage.localStorage.get('ins-paid-products-category-53128') || ''; /*OPT-53128*/
+
+/* Object.keys(Insider.storage.localStorage.get('ins-purchased-control-opt53128').purchasedCategoryOfProduct).includes('Phấn Nước Cushion') */
+Object.keys(Insider.storage.localStorage.get('ins-purchased-control-opt53128').purchasedCategoryOfProduct).join('||');
--- cr2.js
+++ cr2.js
@@ -1,83 +1,124 @@
 /* OPT-53128 START*/
-(function (self) {
-    var productInfosStorage = 'ins-product-infos-53128';
-    var paidProductsCategories = 'ins-paid-products-category-53128';
-    var categoryBought = JSON.parse(Insider.storage.localStorage.get(paidProductsCategories)) || [];
- 
-    self.init = function () {
-        setTimeout(function () {self.checkConditions();}, 2000);
-        
-    };
- 
-    self.checkConditions = function () {
-        if ((Insider.systemRules.call('isOnCartPage') || Insider.fns.hasParameter('/checkouts/'))) {
-            var productUrls = Insider.storageAccessor.paidProducts().map(function (product) {
-                return product.url;
-            });
+var storageData = 'ins-product-infos-53128';
+var storageInformations = {
+    addedItemsInformations: {
+        cats: '',
+        ids: ''
+    },
+    afterAddedItemsInformations: {
+        ids: ''
+    }
+};
+var productDatasArray = [];
 
-            productUrls.forEach(function (url) {
-                Insider.request.get({
-                    url: url,
-                    data: '',
-                    success: function (response) {
-                        var element = document.createElement('div');
-   
-                        element.innerHTML = response.response;
-   
-                        var productInfos = JSON.parse(Insider.storage.localStorage.get(productInfosStorage)) || [];
-                        var categoryText = Insider.dom('.pro-meta--col.category .detail', element).text().trim();
-                        var productIDText = Insider.dom('span#pro_sku', element).text().trim();
-   
-                        var productDatas = {
-                            cat: categoryText,
-                            id: productIDText
-                        };
-   
-                        productInfos.push(productDatas);
-   
-                        Insider.storage.set({
-                            name: productInfosStorage,
-                            value: JSON.stringify(productInfos)
-                        });
-   
-                        JSON.parse(Insider.storage.localStorage.get(productInfosStorage));
-                    }
-                });
-            });
-        }
- 
-        if (Insider.systemRules.call('isOnAfterPaymentPage')) {
-            var productDatas = JSON.parse(Insider.storage.localStorage.get(productInfosStorage));
-            var paidProducts = Insider.systemRules.call('getPaidProducts');
-            var index;
- 
-            for (var i =0; i < productDatas.length; i++) {
-                for (var z = 0; z < paidProducts.length; z++) {
-                    if (productDatas[i].id.indexOf(paidProducts[z].id)) {
-                        index = categoryBought.indexOf(productDatas[i].cat);
- 
-                        if (index > -1) {
-                            categoryBought.splice(index, 1);
-                        }
- 
-                        categoryBought.push(productDatas[i].cat);                 
-                    }
-                }
+if ((Insider.systemRules.call('isOnCartPage') || Insider.fns.hasParameter('/checkouts/'))) {
+    var productUrls = Insider.storageAccessor.paidProducts().map(function (product) {
+        return product.url;
+    });
+
+    productUrls.forEach(function (url) {
+        Insider.request.get({
+            url: url,
+            data: '',
+            success: function (response) {
+                var element = document.createElement('div');
+
+                element.innerHTML = response.response;
+
+                var categoryText = Insider.dom('.pro-meta--col.category .detail', element).text().trim();
+                var productIDText = Insider.dom('span#pro_sku', element).text().trim();
+                var productDatas = {
+                    cat: categoryText,
+                    id: productIDText
+                };
+
+                productDatasArray.push(productDatas);
             }
- 
-            Insider.storage.set({
-                name: paidProductsCategories,
-                value: JSON.stringify(categoryBought),
-                expires: Insider.dateHelper.addDay(90)
-            });
-  
-            JSON.parse(Insider.storage.localStorage.get(paidProductsCategories));
+        });
+    });
+    var storageInformations = {
+        addedItemsInformations: {
+            cats: productDatasArray.cat,
+            ids: productDatasArray.id
         }
     };
- 
-    self.init();
-})({});
+    
+    storageInformations;
+}
+
 /* OPT-53128 END*/
 
 // last_3_months_order_categories
 Insider.storage.localStorage.get('ins-paid-products-category-53128') || ''; /*OPT-53128*/
+
+self.init = function () {
+    setTimeout(function () {
+        self.checkConditions();
+    }, 2000);
+};
+
+self.checkConditions = function () {
+    if ((Insider.systemRules.call('isOnCartPage') || Insider.fns.hasParameter('/checkouts/'))) {
+        var productUrls = Insider.storageAccessor.paidProducts().map(function (product) {
+            return product.url;
+        });
+
+        productUrls.forEach(function (url) {
+            Insider.request.get({
+                url: url,
+                data: '',
+                success: function (response) {
+                    var element = document.createElement('div');
+
+                    element.innerHTML = response.response;
+                    var productInfos = JSON.parse(Insider.storage.localStorage.get(productInfosStorage)) || [];
+                    var categoryText = Insider.dom('.pro-meta--col.category .detail', element).text().trim();
+                    var productIDText = Insider.dom('span#pro_sku', element).text().trim();
+                    var productDatas = {
+                        cat: categoryText,
+                        id: productIDText
+                    };
+
+                    productInfos.push(productDatas);
+
+                    Insider.storage.set({
+                        name: productInfosStorage,
+                        value: JSON.stringify(productInfos)
+                    });
+
+                    JSON.parse(Insider.storage.localStorage.get(productInfosStorage));
+                }
+            });
+        });
+    }
+
+    if (Insider.systemRules.call('isOnAfterPaymentPage')) {
+        var productDatas = JSON.parse(Insider.storage.localStorage.get(productInfosStorage));
+        var paidProducts = Insider.systemRules.call('getPaidProducts');
+        var index;
+
+        for (var i =0; i < productDatas.length; i++) {
+            for (var z = 0; z < paidProducts.length; z++) {
+                if (productDatas[i].id.indexOf(paidProducts[z].id)) {
+                    index = categoryBought.indexOf(productDatas[i].cat);
+
+                    if (index > -1) {
+                        categoryBought.splice(index, 1);
+                    }
+
+                    categoryBought.push(productDatas[i].cat);
+                }
+            }
+        }
+
+        Insider.storage.set({
+            name: paidProductsCategories,
+            value: JSON.stringify(categoryBought),
+            expires: Insider.dateHelper.addDay(90)
+        });
+
+        JSON.parse(Insider.storage.localStorage.get(paidProductsCategories));
+    }
+};
+
+self.init();
