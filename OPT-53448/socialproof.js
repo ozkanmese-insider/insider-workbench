c684055a8064233ac525d55f05345b488df553b7
ozkanmese-insider <77895739+ozkanmese-insider@users.noreply.github.com>
2021-03-11 19:54:38 +0300

    Create socialproof.js

---
+++ OPT-53448/socialproof.js
@@ -0,0 +1,16 @@
+/* OPT-53448 Start */
+var builderId = Insider.browser.isMobile() ? 186 : 185;
+var variationId = Insider.campaign.userSegment.getActiveVariationByBuilderId(builderId);
+var selector = '.ins-preview-wrapper-' + variationId + ' .ins-dynamic-element-tag';
+
+Insider.fns.onElementLoaded(selector, function () {
+    var currentNumber = Number(Insider.dom(selector).text());
+    var newNumber = (currentNumber % 100);
+
+    newNumber = newNumber < 10 ? newNumber * 5 : newNumber;
+
+    Insider.dom(selector).text(newNumber);
+}).listen();
+
+!!Insider.storage.get('ins-test');
+/* OPT-53448 End */
